586045b187552cb543bc5dcd2fe62d12e9a0130f
Arpit Dixit <arpitd@dewsolutions.in>
2021-07-08 23:23:59 +0530

    Initial Commit

---
+++ SwiftUI-BarGraph_Animation/SwiftUI_BarGraph_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BarGraph_AnimationApp.swift
+//  SwiftUI-BarGraph_Animation
+//
+//  Created by Arpit Dixit on 08/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BarGraph_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
